3e047991248d05f31594b8b73d1eb546e4999b16
Eduardo Ponce <edponce2010@gmail.com>
2019-04-24 01:06:00 -0400

    variable changes to install script and added PROFILE flag

--- QuickerUMLS/install.py
+++ QuickerUMLS/install.py
@@ -6,16 +6,22 @@ import argparse
 from toolbox import CuiSemTypesDB, SimstringDBWriter
 from constants import HEADERS_MRCONSO, HEADERS_MRSTY, LANGUAGES
 
+
 try:
     from unidecode import unidecode
     UNIDECODE_AVAIL = True
 except ImportError:
     UNIDECODE_AVAIL = False
 
+# 0 = No status info
+# 1 = General status info
+# 2 = Detailed status info
+PROFILE = 2
 
-def extract_mrsty(mrsty_path, mrsty_headers=HEADERS_MRSTY):
-    sem_types = {}
-    with open(mrsty_path, 'r') as f:
+
+def extract_mrsty(mrsty_file, mrsty_headers=HEADERS_MRSTY):
+    cuisty_dict = {}
+    with open(mrsty_file, 'r') as f:
         # Parse lines only until the fields we need, ignore remaining
         cui_idx = mrsty_headers.index('cui')
         sty_idx = mrsty_headers.index('sty')
@@ -23,15 +29,23 @@ def extract_mrsty(mrsty_path, mrsty_headers=HEADERS_MRSTY):
 
         for ln in f:
             content = ln.strip().split('|', max_split)
-            sem_types.setdefault(content[cui_idx], set()).add(content[sty_idx].encode('utf-8'))
+            sty = content[sty_idx].encode('utf-8')
+            cuisty_dict.setdefault(content[cui_idx], set()).add(sty)
+
+    # Profile
+    if PROFILE > 1:
+        print(f'Num unique CUIs: {len(cuisty_dict)}')
+        print(f'Num values in CUI-STY dictionary: {sum(len(v) for v in cuisty_dict.values())}')
+        print(f'Size of CUI-STY dictionary: {sys.getsizeof(cuisty_dict)}')
+
     return sem_types
 
 
-def extract_mrconso(mrconso_path, sem_types, mrconso_header=HEADERS_MRCONSO,
+def extract_mrconso(mrconso_file, sem_types, mrconso_header=HEADERS_MRCONSO,
                     lowercase=False,
                     normalize_unicode=False,
                     language=['ENG']):
-    with open(mrconso_path, 'r') as f:
+    with open(mrconso_file, 'r') as f:
         # Parse lines only until the fields we need, ignore remaining
         str_idx = mrconso_header.index('str')
         cui_idx = mrconso_header.index('cui')
@@ -79,11 +93,12 @@ def extract_mrconso(mrconso_path, sem_types, mrconso_header=HEADERS_MRCONSO,
             yield (text, cui, sem_types[cui], preferred)
 
         # Profile
-        print(f'Num lines to process: {num_lines}')
-        print(f'Num valid language: {num_valid_lang}')
-        print(f'Num repeated CUI-term: {num_repeated_cuitext}')
-        print(f'Num processed: {len(processed)}')
-        print(f'Size of processed CUI-term: {sys.getsizeof(processed)}')
+        if PROFILE > 1:
+            print(f'Num lines to process: {num_lines}')
+            print(f'Num valid language: {num_valid_lang}')
+            print(f'Num repeated CUI-term: {num_repeated_cuitext}')
+            print(f'Num processed: {len(processed)}')
+            print(f'Size of processed CUI-term: {sys.getsizeof(processed)}')
 
 
 def dump_conso_sty(extracted_it, cuisty_dir,
@@ -112,7 +127,7 @@ def dump_conso_sty(extracted_it, cuisty_dir,
             sty_bulk.append((cui, stys))
 
         # Profile
-        if i % status_step == 0:
+        if PROFILE > 1 and i % status_step == 0:
             curr_time = time.time()
             print(f'{i}: {curr_time - prev_time} s, {(curr_time - prev_time) / status_step} s/term')
             prev_time = curr_time
@@ -125,12 +140,14 @@ def dump_conso_sty(extracted_it, cuisty_dir,
         sty_bulk = []
 
         # Profile
-        curr_time = time.time()
-        print(f'{i}: {curr_time - prev_time} s, {(curr_time - prev_time) / status_step} s/term')
+        if PROFILE > 1:
+            curr_time = time.time()
+            print(f'{i}: {curr_time - prev_time} s, {(curr_time - prev_time) / status_step} s/term')
 
     # Profile
-    print(f'Num terms: {num_terms}')
-    print(f'Num unique terms: {len(terms)}')
+    if PROFILE > 0:
+        print(f'Num terms: {num_terms}')
+        print(f'Num unique terms: {len(terms)}')
 
     return terms
 
@@ -145,7 +162,7 @@ def dump_terms(simstring_dir, simstring_terms,
         ss_db.insert(term)
 
         # Profile
-        if i % status_step == 0:
+        if PROFILE > 1 and i % status_step == 0:
             curr_time = time.time()
             print(f'{i}: {curr_time - prev_time} s, {(curr_time - prev_time) / status_step} s/term')
             prev_time = curr_time
@@ -153,28 +170,25 @@ def dump_terms(simstring_dir, simstring_terms,
 
 def driver(opts):
     # UMLS files
-    mrconso_path = os.path.join(opts.umls_installation_path, 'MRCONSO.RRF')
-    mrsty_path = os.path.join(opts.umls_installation_path, 'MRSTY.RRF')
+    mrconso_file = os.path.join(opts.umls_dir, 'MRCONSO.RRF')
+    mrsty_file = os.path.join(opts.umls_dir, 'MRSTY.RRF')
 
-    # Create destination directories for the two databases
-    simstring_dir = os.path.join(opts.destination_path, 'umls-simstring.db')
-    cuisty_dir = os.path.join(opts.destination_path, 'cui-semtypes.db')
+    # Create install directories for the two databases
+    simstring_dir = os.path.join(opts.install_dir, 'umls-simstring.db')
+    cuisty_dir = os.path.join(opts.install_dir, 'cui-semtypes.db')
     os.makedirs(simstring_dir)
     os.makedirs(cuisty_dir)
 
     print('Loading semantic types...')
     start = time.time()
-    cuisty_dict = extract_mrsty(mrsty_path)
+    cuisty_dict = extract_mrsty(mrsty_file)
     curr_time = time.time()
     print(f'Loading semantic types: {curr_time - start} s')
-    print(f'Num unique CUIs: {len(cuisty_dict)}')
-    print(f'Num values in CUI-STY dictionary: {sum(len(v) for v in cuisty_dict.values())}')
-    print(f'Size of CUI-STY dictionary: {sys.getsizeof(cuisty_dict)}')
 
     print('Loading and parsing concepts...')
     start = time.time()
     conso_cuisty_iter = extract_mrconso(
-                            mrconso_path, cuisty_dict,
+                            mrconso_file, cuisty_dict,
                             lowercase=opts.lowercase,
                             normalize_unicode=opts.normalize_unicode,
                             language=opts.language)
@@ -190,48 +204,35 @@ def driver(opts):
 
 
 if __name__ == '__main__':
-    ap = argparse.ArgumentParser()
-    ap.add_argument(
-        'umls_installation_path',
-        help=('Location of UMLS installation files (`MRCONSO.RRF` and '
-              '`MRSTY.RRF` files)')
+    args = argparse.ArgumentParser()
+    args.add_argument(
+        '-U', '--umls_dir',
+        help='Directory of UMLS RRF files'
     )
-    ap.add_argument(
-        'destination_path',
-        help='Location where the necessary QuickUMLS files are installed'
+    args.add_argument(
+        '-I', '--install_dir',
+        help='Directory for installing QuickerUMLS files'
     )
-    ap.add_argument(
+    args.add_argument(
         '-L', '--lowercase', action='store_true',
         help='Consider only lowercase version of tokens'
     )
-    ap.add_argument(
+    args.add_argument(
         '-N', '--normalize-unicode', action='store_true',
         help='Normalize unicode strings to their closest ASCII representation'
     )
-    ap.add_argument(
+    args.add_argument(
         '-E', '--language', default=['ENG'], action='append', choices=LANGUAGES,
         help='Extract concepts of the specified language'
     )
-    clargs = ap.parse_args()
+    clargs = args.parse_args()
 
-    if not os.path.exists(clargs.destination_path):
-        msg = ('directory "{}" does not exists; should i create it? [y/n] '
-               ''.format(clargs.destination_path))
-        create = input(msg).lower().strip() == 'y'
-        if not create:
-            print('aborting.')
-            exit(1)
-        os.makedirs(clargs.destination_path)
-
-    if len(os.listdir(clargs.destination_path)) > 0:
-        msg = ('directory "{}" is not empty; should i empty it? [y/n] '
-               ''.format(clargs.destination_path))
-        empty = input(msg).lower().strip() == 'y'
-        if not empty:
-            print('aborting.')
-            exit(1)
-        shutil.rmtree(clargs.destination_path)
-        os.makedirs(clargs.destination_path)
+    if not os.path.exists(clargs.install_dir):
+        print(f'Creating install directory: {clargs.install_dir}')
+        os.makedirs(clargs.install_dir)
+    elif len(os.listdir(clargs.install_dir)) > 0:
+        print(f'Install directory ({clargs.install_dir}) is not empty...aborting')
+        exit(1)
 
     if clargs.normalize_unicode:
         if not UNIDECODE_AVAIL:
@@ -240,14 +241,14 @@ if __name__ == '__main__':
                    command.""")
             print(err, file=sys.stderr)
             exit(1)
-        flag_fp = os.path.join(clargs.destination_path, 'normalize-unicode.flag')
+        flag_fp = os.path.join(clargs.install_dir, 'normalize-unicode.flag')
         open(flag_fp, 'w').close()
 
     if clargs.lowercase:
-        flag_fp = os.path.join(clargs.destination_path, 'lowercase.flag')
+        flag_fp = os.path.join(clargs.install_dir, 'lowercase.flag')
         open(flag_fp, 'w').close()
 
-    flag_fp = os.path.join(clargs.destination_path, 'language.flag')
+    flag_fp = os.path.join(clargs.install_dir, 'language.flag')
     with open(flag_fp, 'w') as f:
         f.write(os.linesep.join(clargs.language))
 
