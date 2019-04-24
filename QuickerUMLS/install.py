9bcfc4ec5daeb795f29d73e0234c6a81b286e95a
Eduardo Ponce <edponce2010@gmail.com>
2019-04-23 22:37:34 -0400

    added profiling to install script

--- QuickerUMLS/install.py
+++ QuickerUMLS/install.py
@@ -3,8 +3,7 @@ import sys
 import time
 import shutil
 import argparse
-from six.moves import input
-from toolbox import CuiSemTypesDB, SimstringDBWriter, mkdir
+from toolbox import CuiSemTypesDB, SimstringDBWriter
 from constants import HEADERS_MRCONSO, HEADERS_MRSTY, LANGUAGES
 
 try:
@@ -14,7 +13,7 @@ except ImportError:
     UNIDECODE_AVAIL = False
 
 
-def get_semantic_types(mrsty_path, mrsty_headers):
+def extract_mrsty(mrsty_path, mrsty_headers=HEADERS_MRSTY):
     sem_types = {}
     with open(mrsty_path, 'r') as f:
         # Parse lines only until the fields we need, ignore remaining
@@ -28,7 +27,10 @@ def get_semantic_types(mrsty_path, mrsty_headers):
     return sem_types
 
 
-def extract_from_mrconso(mrconso_path, sem_types, opts, mrconso_header):
+def extract_mrconso(mrconso_path, sem_types, mrconso_header=HEADERS_MRCONSO,
+                    lowercase=False,
+                    normalize_unicode=False,
+                    language=['ENG']):
     with open(mrconso_path, 'r') as f:
         # Parse lines only until the fields we need, ignore remaining
         str_idx = mrconso_header.index('str')
@@ -38,41 +40,67 @@ def extract_from_mrconso(mrconso_path, sem_types, opts, mrconso_header):
         max_split = max(str_idx, cui_idx, pref_idx, lat_idx) + 1
 
         processed = set()
+
+        # Profile
+        num_valid_lang = 0
+        num_repeated_cuitext = 0
+        num_lines = 0
+
         for ln in f:
+            # Profile
+            num_lines += 1
+
             content = ln.strip().split('|', max_split)
 
-            if content[lat_idx] != opts.language:
+            if content[lat_idx] in language:
                 continue
+
+            # Profile
+            num_valid_lang += 1
 
             cui = content[cui_idx]
             text = content[str_idx].strip()
 
             if (cui, text) in processed:
+                # Profile
+                num_repeated_cuitext += 1
                 continue
 
             processed.add((cui, text))
 
-            if opts.lowercase:
+            if lowercase:
                 text = text.lower()
 
-            if opts.normalize_unicode:
+            if normalize_unicode:
                 text = unidecode(text)
 
             preferred = 1 if content[pref_idx] else 0
 
             yield (text, cui, sem_types[cui], preferred)
 
+        # Profile
+        print(f'Num lines to process: {num_lines}')
+        print(f'Num valid language: {num_valid_lang}')
+        print(f'Num repeated CUI/term: {num_repeated_cuitext}')
+        print(f'Num processed: {len(processed)}')
+        print(f'Size of processed CUI/term: {sys.getsizeof(processed)}')
 
-def parse_and_encode_ngrams(extracted_it, cuisty_dir, bulk_size=1000, status_step=100000):
+
+def dump_conso_sty(extracted_it, cuisty_dir,
+                   bulk_size=1000, status_step=100000):
     # Profile
     prev_time = time.time()
+    num_terms = 0
 
     cuisty_db = CuiSemTypesDB(cuisty_dir)
-    simstring_terms = set()
+    terms = set()
     cui_bulk = []
     sty_bulk = []
     for i, (term, cui, stys, preferred) in enumerate(extracted_it, start=1):
-        simstring_terms.add(term)
+        # Profile
+        num_terms += 1
+
+        terms.add(term)
 
         if len(cui_bulk) == bulk_size:
             cuisty_db.bulk_insert_cui(cui_bulk)
@@ -102,10 +130,15 @@ def parse_and_encode_ngrams(extracted_it, cuisty_dir, bulk_size=1000, status_ste
         print(f'{i}: {curr_time - prev_time} s, {(curr_time - prev_time) / status_step} s/term')
         sys.stdout.flush()
 
-    return simstring_terms
+    # Profile
+    print(f'Number of terms: {num_terms}')
+    print(f'Number of unique terms: {len(terms)}')
+
+    return terms
 
 
-def dump_simstrings(simstring_dir, simstring_terms, status_step=10000):
+def dump_terms(simstring_dir, simstring_terms,
+               status_step=100000):
     # Profile
     prev_time = time.time()
 
@@ -129,33 +162,36 @@ def driver(opts):
     # Create destination directories for the two databases
     simstring_dir = os.path.join(opts.destination_path, 'umls-simstring.db')
     cuisty_dir = os.path.join(opts.destination_path, 'cui-semtypes.db')
-    mkdir(simstring_dir)
-    mkdir(cuisty_dir)
+    os.makedirs(simstring_dir)
+    os.makedirs(cuisty_dir)
 
     print('Loading semantic types...')
     sys.stdout.flush()
     start = time.time()
-    sem_types = get_semantic_types(mrsty_path, HEADERS_MRSTY)
+    cuisty_dict = extract_mrsty(mrsty_path)
     curr_time = time.time()
     print(f'Loading semantic types: {curr_time - start} s')
-    print(f'Number of unique CUIs: {len(sem_types)}')
+    print(f'Number of unique CUIs: {len(cuisty_dict)}')
+    print(f'Size of CUI/STY dictionary: {sys.getsizeof(cuisty_dict)}')
 
     print('Loading and parsing concepts...')
     sys.stdout.flush()
     start = time.time()
-    mrconso_iterator = extract_from_mrconso(mrconso_path, sem_types, opts, HEADERS_MRCONSO)
-    simstring_terms = parse_and_encode_ngrams(mrconso_iterator, cuisty_dir)
+    conso_cuisty_iter = extract_mrconso(
+                            mrconso_path, cuisty_dict,
+                            lowercase=opts.lowercase,
+                            normalize_unicode=opts.normalize_unicode,
+                            language=opts.language)
+    terms = dump_conso_sty(conso_cuisty_iter, cuisty_dir)
     curr_time = time.time()
     print(f'Loading and parsing concepts: {curr_time - start} s')
-    print(f'Number of unique (CUI, term): {len(simstring_terms)}')
 
     print('Writing Simstring database...')
     sys.stdout.flush()
     start = time.time()
-    dump_simstrings(simstring_dir, simstring_terms)
+    dump_terms(simstring_dir, terms)
     curr_time = time.time()
     print(f'Writing Simstring database: {curr_time - start} s')
-
 
 
 if __name__ == '__main__':
@@ -174,50 +210,50 @@ if __name__ == '__main__':
         help='Consider only lowercase version of tokens'
     )
     ap.add_argument(
-        '-U', '--normalize-unicode', action='store_true',
+        '-N', '--normalize-unicode', action='store_true',
         help='Normalize unicode strings to their closest ASCII representation'
     )
     ap.add_argument(
-        '-E', '--language', default='ENG', choices=LANGUAGES,
+        '-E', '--language', default=['ENG'], action='append', choices=LANGUAGES,
         help='Extract concepts of the specified language'
     )
-    opts = ap.parse_args()
+    clargs = ap.parse_args()
 
-    if not os.path.exists(opts.destination_path):
+    if not os.path.exists(clargs.destination_path):
         msg = ('directory "{}" does not exists; should i create it? [y/n] '
-               ''.format(opts.destination_path))
+               ''.format(clargs.destination_path))
         create = input(msg).lower().strip() == 'y'
         if not create:
             print('aborting.')
             exit(1)
-        os.makedirs(opts.destination_path)
+        os.makedirs(clargs.destination_path)
 
-    if len(os.listdir(opts.destination_path)) > 0:
+    if len(os.listdir(clargs.destination_path)) > 0:
         msg = ('directory "{}" is not empty; should i empty it? [y/n] '
-               ''.format(opts.destination_path))
+               ''.format(clargs.destination_path))
         empty = input(msg).lower().strip() == 'y'
         if not empty:
             print('aborting.')
             exit(1)
-        shutil.rmtree(opts.destination_path)
-        os.mkdir(opts.destination_path)
+        shutil.rmtree(clargs.destination_path)
+        os.makedirs(clargs.destination_path)
 
-    if opts.normalize_unicode:
+    if clargs.normalize_unicode:
         if not UNIDECODE_AVAIL:
             err = ("""'unidecode' is needed for unicode normalization
                    please install it via the 'pip install unidecode'
                    command.""")
             print(err, file=sys.stderr)
             exit(1)
-        flag_fp = os.path.join(opts.destination_path, 'normalize-unicode.flag')
+        flag_fp = os.path.join(clargs.destination_path, 'normalize-unicode.flag')
         open(flag_fp, 'w').close()
 
-    if opts.lowercase:
-        flag_fp = os.path.join(opts.destination_path, 'lowercase.flag')
+    if clargs.lowercase:
+        flag_fp = os.path.join(clargs.destination_path, 'lowercase.flag')
         open(flag_fp, 'w').close()
 
-    flag_fp = os.path.join(opts.destination_path, 'language.flag')
+    flag_fp = os.path.join(clargs.destination_path, 'language.flag')
     with open(flag_fp, 'w') as f:
-        f.write(opts.language)
+        f.write(os.linesep.join(clargs.language))
 
-    driver(opts)
+    driver(clargs)
